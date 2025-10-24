c517341d6a9304cb7ef813fa2fb815b58622fda4
hammadtufail7 <hammadanjum938@gmail.com>
2025-10-24 19:57:25 +0500

    first commit

---
+++ sptoify_clone.js
@@ -0,0 +1,19 @@
+ var swiper = new Swiper(".swiper", {
+      slidesPerView: 5,
+      centeredSlides: false,
+      spaceBetween: 0,
+      fade:'true',
+      grabCursor:'true',
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+
+
+
